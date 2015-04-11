317d359daa57553395c5ead13e93c737b448470b
Smac89 <smac89@users.noreply.github.com>
2015-04-10 23:50:42 -0600

    Started writing the algorithm for kdtree

---
+++ solutions/classic-algorithms/closestpair.cpp
@@ -0,0 +1,15 @@
+#include <initializer_list>
+#include <tuple>
+
+namespace spatial {
+	class kdtree {
+		public:
+			kdtree(std::initializer_list<double> &list) {
+				
+			}
+	};
+}
+
+int main() {
+
+}
