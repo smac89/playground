3bc7d7da409405de25d67af6e95cf8f71d2567f7
Smac89 <smac89@users.noreply.github.com>
2015-04-11 01:52:14 -0600

    Finished the constructors for the tree and nodes

--- solutions/classic-algorithms/closestpair.cpp
+++ solutions/classic-algorithms/closestpair.cpp
@@ -1,15 +1,26 @@
-#include <initializer_list>
-#include <tuple>
+#include "closestpair.hpp"
+#include "node.hpp"
+#include <vector>
+#include <iterator>
+#include <algorithm>
 
 namespace spatial {
-	class kdtree {
-		public:
-			kdtree(std::initializer_list<double> &list) {
-				
+	kdtree::kdtree(std::vector<kdnode> &list) {
+		int cardinal = 0;
+		for (kdnode& node : list) {
+			if (cardinal == 0) cardinal = node.getCardinal();
+			if (cardinal != node.getCardinal()) {
+				throw std::invalid_argument("All nodes in the list must be of the same type");
 			}
-	};
+		}
+		std::copy(list.begin(), list.end(), std::back_inserter(points));
+	}
+
+	void sortTree(kdtree &tree) {
+
+	}
 }
 
 int main() {
-
+	spatial::kdnode2D node({8.9, 9.9});
 }
