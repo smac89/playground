113207348b69a7c7725845ed04c3f65fa404cb8c
Smac89 <smac89@users.noreply.github.com>
2015-04-12 13:27:54 -0600

    Fixed indentation of all files to use space

--- solutions/classic-algorithms/closestpair/closestpair.cpp
+++ solutions/classic-algorithms/closestpair/closestpair.cpp
@@ -6,15 +6,14 @@
 using namespace spatial;
 
 
-
 int main() {
-	std::vector<kdNode<2>*> nodes = generateNodes<2>(10);
-	for (kdNode<2> *node : nodes) {
-		std::cout << *node << std::endl;
-	}
+    std::vector<kdNode<2>*> nodes = generateNodes<2>(10);
+    for (kdNode<2> *node : nodes) {
+        std::cout << *node << std::endl;
+    }
 
-	kdTree<2> tree(nodes);
-	std::pair<kdNode<2>*, kdNode<2>*> nearest = tree.closestPair();
-	std::cout << std::endl << std::endl << *(nearest.first) << " " << *(nearest.second) << std::endl;
-	return 0;
+    kdTree<2> tree(nodes);
+    std::pair<kdNode<2>*, kdNode<2>*> nearest = tree.closestPair();
+    std::cout << std::endl << std::endl << *(nearest.first) << " " << *(nearest.second) << std::endl;
+    return 0;
 }
--- solutions/classic-algorithms/closestpair/node.hpp
+++ solutions/classic-algorithms/closestpair/node.hpp
@@ -9,52 +9,52 @@
 #include <type_traits>
 
 namespace spatial {
-	template <std::size_t N> class kdTree;
+    template <std::size_t N> class kdTree;
 
-	template <std::size_t N>
-	struct kdNode  {
+    template <std::size_t N>
+    struct kdNode  {
 
-		explicit kdNode(const std::array<double, N> &coords) {
-			if (N <= 1) {
-				throw std::invalid_argument("The cardinality has to be greater than 1");
-			}
-			this->coords = new double[N];
-			std::copy(coords.begin(), coords.end(), this->coords);
-		}
+        explicit kdNode(const std::array<double, N> &coords) {
+            if (N <= 1) {
+                throw std::invalid_argument("The cardinality has to be greater than 1");
+            }
+            this->coords = new double[N];
+            std::copy(coords.begin(), coords.end(), this->coords);
+        }
 
-		template <std::size_t T>
-		friend std::ostream& operator << (std::ostream& oss, const kdNode<T>& node) {
-			oss << "{" << node.coords[0];
-			for (int t = 1; t < N; t++) {
-				oss << ", " << node.coords[t];
-			}
-			oss << "}";
-			return oss;
-		}
+        template <std::size_t T>
+        friend std::ostream& operator << (std::ostream& oss, const kdNode<T>& node) {
+            oss << "{" << node.coords[0];
+            for (int t = 1; t < N; t++) {
+                oss << ", " << node.coords[t];
+            }
+            oss << "}";
+            return oss;
+        }
 
-		virtual ~kdNode() {
-			delete []coords;
-		}
+        virtual ~kdNode() {
+            delete []coords;
+        }
 
 
-	protected:
-		double getDim(int dim) const {
-			return coords[dim];
-		}
+    protected:
+        double getDim(int dim) const {
+            return coords[dim];
+        }
 
-		double distTo(kdNode<N> &other) const {
-			int index = 0;
-			double sump = std::accumulate(coords, &coords[N], 0,
-				[&](double accum, double next) {
-					return accum + std::pow((next - other.coords[index++]), 2);
-				}
-			);
-			return std::sqrt(sump);
-		}
+        double distTo(kdNode<N> &other) const {
+            int index = 0;
+            double sump = std::accumulate(coords, &coords[N], 0,
+                [&](double accum, double next) {
+                    return accum + std::pow((next - other.coords[index++]), 2);
+                }
+            );
+            return std::sqrt(sump);
+        }
 
-		double *coords;
-		friend class kdTree<N>;
-	};
+        double *coords;
+        friend class kdTree<N>;
+    };
 }
 
 #endif // __KDNODE__
