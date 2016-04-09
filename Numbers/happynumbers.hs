f924c028cc75ee35cef293015f9cc2691409809c
smac89 <smac89@users.noreply.github.com>
2016-04-09 14:44:18 -0600

    Finished happy numbers

--- Numbers/happynumbers.hs
+++ Numbers/happynumbers.hs
@@ -1,16 +1,13 @@
-{-- |
-let n be in the range 10^0 to 10^k where k >= 0
-1. Find the first number h which when squared is not greater than n
-2. If such a number does not exist, go to the next value of n and start at step 1
-3. Determine if (n - h^2) is a perfect square; if it is, then output h and the square root of the difference
-computed, then go to step 1
-4. Otherwise repeat step 1 -}
+import Data.Char (digitToInt)
 
 main :: IO ()
 main = do
-	num <- putStr "How many happy numbers to find? " >> readLn :: IO Int
-	happy num
+	numbers <- putStr "How many happy numbers to find? " >> readLn :: IO Int
+	print $ happy numbers
 
-happy :: Int -> IO ()
-happy n | n <= 0 = putStr ""
-		| otherwise = ???
+happy :: Int -> [Integer]
+happy n = take n $ filter isHappy [1..]
+	where isHappy n | n == 4 = False
+					| n == 1 = True
+			  		| otherwise = isHappy $ reduce n
+			  		where reduce = floor . (foldr (+) 0).(map $ (^ 2) . fromIntegral . digitToInt).show
