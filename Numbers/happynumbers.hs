01504126edfedc7d817ff8b7540030c62d9ec1f0
smac89 <smac89@users.noreply.github.com>
2016-04-09 15:33:20 -0600

    Can use add rather than foldr

--- Numbers/happynumbers.hs
+++ Numbers/happynumbers.hs
@@ -10,4 +10,4 @@ happy = (`take` (filter isHappy [1..]))
 	where isHappy n | n == 4 = False
 					| n == 1 = True
 			  		| otherwise = isHappy $ reduce n
-			  		where reduce = floor . (foldr (+) 0).(map $ (^ 2) . fromIntegral . digitToInt).show
+			  		where reduce = sum . (map $ (^ 2).toInteger.digitToInt) . show
