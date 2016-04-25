4723aba666ecb4077542feba52bda2659606c5ab
smac89 <smac89@users.noreply.github.com>
2016-04-24 22:07:44 -0600

    Started working on towers problem. Also added some stuff to gitignore

---
+++ kattis/towers.py
@@ -0,0 +1,23 @@
+import sys
+import operator
+import math
+from decimal import Decimal, getcontext, ROUND_DOWN
+
+def domath(cnt):
+    lines = [sys.stdin.next().strip() for _ in xrange(cnt)]
+    values = {line : (fake_exponent(map(int, line.split("^"))), cnt) for cnt, line in enumerate(lines)}
+    print values
+    return "\n".join(sorted(lines, key=values.__getitem__))
+
+def fake_exponent(l):
+    exp = reduce(operator.mul, (l[v] for v in xrange(1, len(l))), 1)
+    base = l[0]
+    ans = exponents[base - 1] * Decimal(exp)
+    print l, ans.quantize(Decimal('.001'), rounding=)
+    return ans
+
+if __name__ == '__main__':
+    # getcontext().prec = 220
+    global exponents
+    exponents = [Decimal(v + 1).ln() for v in xrange(100)]
+    print "\n".join("Case %d:\n%s" %(case + 1, domath(int(cnt))) for case, cnt in enumerate(sys.stdin))
