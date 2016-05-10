8bf086370d850ca2d17d7dfc31215290d94b6c52
smac89 <smac89@users.noreply.github.com>
2016-05-09 23:36:47 -0600

    Updated changes to towers.py file

--- kattis/towers.py
+++ kattis/towers.py
@@ -1,42 +1,37 @@
 import sys
-import operator
 import math
-from decimal import Decimal, getcontext, ROUND_DOWN
+import operator
 
 class PowerTower(object):
-    __slots__ = ('tower')
+    __slots__ = ('value', 'tower')
     
     def __init__(self, tower):
-        self.tower = tuple(map(int, tower.split("^")))
+        self.tower = map(int, tower.split("^"))
+        self.tower = [math.log(self.tower[i]) for i in xrange(len(self.tower) - 1)] + [self.tower[-1]]
+        self.value = reduce(operator.mul, self.tower)
 
     def __lt__(self, other):
-        return PowerTower.approximate(self.tower, other.tower)
-
-    @staticmethod
-    def approximate(lhs, rhs):
-        def evalp(sl, sr):
-            return (0, 0)
-
-        v, w = evalp(1, 1)
-        return v / math.log(rhs[0]) < w / math.log(lhs[0])
-
+        val_l, val_r = self.value, other.value
+        if len(self.tower) < len(other.tower):
+            for _ in xrange(len(other.tower) - len(self.tower)):
+                val_l = math.log(val_l)
+        elif len(self.tower) > len(other.tower):
+            for _ in xrange(len(self.tower) - len(other.tower)):
+                val_r = math.log(val_r)
+        return self.val_l < val_r
 
 
 def domath(cnt):
     lines = [sys.stdin.next().strip() for _ in xrange(cnt)]
-    values = {line : (fake_exponent(map(int, line.split("^"))), cnt) for cnt, line in enumerate(lines)}
-    print values
+    values = {line : (PowerTower(line), cnt) for cnt, line in enumerate(lines)}
+    # print values
     return "\n".join(sorted(lines, key=values.__getitem__))
 
 def fake_exponent(l):
-    exp = reduce(operator.mul, (l[v] for v in xrange(1, len(l))), 1)
-    base = l[0]
-    ans = exponents[base - 1] * Decimal(exp)
-    print l, ans.quantize(Decimal('.001'), rounding=)
-    return ans
+    ans = reduce(reduction, (l[v] for v in xrange(len(l) - 1)), 1)
+    return ans * l[-1]
 
 if __name__ == '__main__':
-    # getcontext().prec = 220
-    global exponents
-    exponents = [Decimal(v + 1).ln() for v in xrange(100)]
+    global reduction
+    reduction = lambda x, y: math.log(x) * math.log(y)
     print "\n".join("Case %d:\n%s" %(case + 1, domath(int(cnt))) for case, cnt in enumerate(sys.stdin))
