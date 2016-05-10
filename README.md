fe6ea8acd08ccf1fbb2fcc6e1e23fa77d0df6319
smac89 <smac89@users.noreply.github.com>
2016-05-09 22:57:08 -0600

    Removed the unneeded files

--- README.md
+++
@@ -1,2 +0,0 @@
-# spotify challenge
-Attempt at solving some of the challenges laid out by spotify [here](https://spotify.kattis.com/)
--- apparatus.py
+++
@@ -1,7 +0,0 @@
-from math import factorial as fac
-
-if __name__ == '__main__':
-    n, m = map(int, raw_input().split())
-    seen = set()
-    for _ in xrange(m):
-        r1, r2 = (raw_input(), raw_input())
