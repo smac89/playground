6cf7d77dfb0bdda50e4e1c2fa067677f3f66aec9
smac89 <smac89@users.noreply.github.com>
2016-12-08 21:01:57 -0600

    Updated cpp template

--- solution.cpp
+++ solution.cpp
@@ -2,8 +2,11 @@
 #include <cstdlib>
 
 int main() {
-    std::cin.tie(nullptr);
+	
+    // Remove synchronization between cstdio and iostream
     std::ios_base::sync_with_stdio(false);
+    // Prevent flushing of output stream before each io operation
+    std::cin.tie(nullptr);
 
     return EXIT_SUCCESS;
 }
